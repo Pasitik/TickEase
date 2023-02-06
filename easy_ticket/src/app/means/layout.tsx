91115c392ecb7afd62bd23c5ed0c76b9fb25d462
Pasitik <paasitik@gmail.com>
2023-02-06 15:24:16 +0000

    App.tsx

---
+++ easy_ticket/src/app/means/layout.tsx
@@ -0,0 +1,26 @@
+import '../globals.css'
+import Nav from '../Nav'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body>
+        <div className=''>
+        <div className=' absolute z-10'>
+            <Nav/>
+        </div>
+        <div>{children}</div>
+        </div>
+        </body>
+    </html>
+  )
+}
